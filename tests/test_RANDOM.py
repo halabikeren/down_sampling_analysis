6241fc76d5234acfc3073dcebc103f0d87336366
halabikeren <halabikeren@gmail.com>
2021-01-27 16:42:32 +0200

    Added a test

--- tests/test_RANDOM.py
+++ tests/test_RANDOM.py
@@ -17,6 +17,14 @@ class TestRANDOM(unittest.TestCase):
         accepted_sample = random_sampler.sample_members
         self.assertEqual(len(accepted_sample), 1)
 
+    def test_sample_no_repeats(self):
+        sequence_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+        random_sampler = RANDOM(all_sequences_path=sequence_data_path)
+        random_sampler.compute_sample(3)
+        accepted_sample = set(random_sampler.sample_members)
+        self.assertEqual(len(accepted_sample), 3)
+
+
     def test_sample_all(self):
         sequence_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
         random_sampler = RANDOM(all_sequences_path=sequence_data_path)
