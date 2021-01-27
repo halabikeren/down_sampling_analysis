642c406ee0cf27713bde9551d97834fa70608901
halabikeren <halabikeren@gmail.com>
2021-01-27 16:41:20 +0200

    Added implementation of random sampling

---
+++ tests/test_RANDOM.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+from pydantic import ValidationError
+from samplers import RANDOM
+
+
+class TestRANDOM(unittest.TestCase):
+
+    def test_class_validation(self):
+        with self.assertRaises(ValidationError):
+            random_sampler = RANDOM(all_sequences_path=None)
+
+    def test_sample_one(self):
+        sequence_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+        random_sampler = RANDOM(all_sequences_path=sequence_data_path)
+        random_sampler.compute_sample(1)
+        accepted_sample = random_sampler.sample_members
+        self.assertEqual(len(accepted_sample), 1)
+
+    def test_sample_all(self):
+        sequence_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+        random_sampler = RANDOM(all_sequences_path=sequence_data_path)
+        random_sampler.compute_sample(5)
+        expected_sample = {"A", "B", "C", "D", "E"}
+        accepted_sample = set(random_sampler.sample_members)
+        self.assertEqual(accepted_sample, expected_sample)
+
+
+if __name__ == '__main__':
+    unittest.main()
