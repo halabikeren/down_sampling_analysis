cc01fce0fc54cf0758baea3872e87faa4a3a47e9
halabikeren <halabikeren@gmail.com>
2021-02-02 13:49:10 +0200

    Added a test for the method to compute taxa weights

--- tests/test_Pda.py
+++ tests/test_Pda.py
@@ -9,7 +9,9 @@ from ete3 import Tree
 
 class TestPDA(unittest.TestCase):
 
-    sequences_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+    sequences_data_path = (
+        f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+    )
     aux_dir = f"{os.path.dirname(os.path.realpath(__file__))}/aux/pda/"
     tree = Tree("((A:2,B:1):1,(C:1,(D:8,E:2):2):2);")
 
@@ -30,7 +32,7 @@ class TestPDA(unittest.TestCase):
         pda = Pda(
             sequences_path=self.sequences_data_path,
             tree=self.tree,
-            aux_dir=self.aux_dir
+            aux_dir=self.aux_dir,
         )
         expected_sample = {"A", "D", "E"}
         accepted_sample = pda.get_sample(3, use_external=True)
@@ -73,7 +75,7 @@ class TestPDA(unittest.TestCase):
             sequences_path=self.sequences_data_path,
             tree=self.tree,
             aux_dir=self.aux_dir,
-            taxon_to_weight={"A": 0.5, "B": 0.5, "C": 1, "D": 0.01, "E": 0.5}
+            taxon_to_weight={"A": 0.5, "B": 0.5, "C": 1, "D": 0.01, "E": 0.5},
         )
         expected_sample = {"A", "D", "E"}
         accepted_sample = pda.get_sample(3, is_weighted=True)
@@ -125,6 +127,15 @@ class TestPDA(unittest.TestCase):
             accepted_sample = list(SeqIO.parse(accepted_sample, "fasta"))
         accepted_sample = set([record.id for record in accepted_sample])
         self.assertEqual(accepted_sample, expected_sample)
+
+    def test_computed_weights(self):
+        pda = Pda(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
+        pda.compute_taxon_weights(f"{os.path.dirname(os.path.realpath(__file__))}/data/aligned_seq_data.fas")
+        self.assertEqual(pda.taxon_to_weight, {'A': 0.68359375, 'B': 0.68359375, 'C': 0.3645833333333333, 'D': 0.5911458333333334, 'E': 0.7356770833333334, 'F': 0.7356770833333334, 'G': 0.7356770833333334, 'H': 0.7356770833333334})
 
 
 if __name__ == "__main__":
