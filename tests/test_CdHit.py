fd92be35fa6a7f85350ec7b8b67a0fb9a28b2cc0
halabikeren <halabikeren@gmail.com>
2021-02-02 13:49:58 +0200

    Updated testers

--- tests/test_CdHit.py
+++ tests/test_CdHit.py
@@ -9,14 +9,18 @@ from ete3 import Tree
 
 class TestCDHIT(unittest.TestCase):
 
-    sequences_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+    sequences_data_path = (
+        f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+    )
     aux_dir = f"{os.path.dirname(os.path.realpath(__file__))}/aux/cdhit/"
     tree = Tree("((A:2,B:1):1,(C:1,(D:8,E:2):2):2);")
 
     def test_sample_one(self):
-        cdhit = CdHit(sequences_path=self.sequences_data_path,
-                      tree=self.tree,
-                      aux_dir=self.aux_dir)
+        cdhit = CdHit(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
         expected_sample = {"A"}
         accepted_sample = cdhit.get_sample(1)
         if type(accepted_sample) is str:
@@ -25,25 +29,23 @@ class TestCDHIT(unittest.TestCase):
         self.assertEqual(accepted_sample, expected_sample)
 
     def test_sample_all(self):
-        cdhit = CdHit(sequences_path=self.sequences_data_path,
-                      tree=self.tree,
-                      aux_dir=self.aux_dir)
-        expected_sample = {"C", "D", "E"}
-        accepted_sample = cdhit.get_sample(4)
+        cdhit = CdHit(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
+        accepted_sample = cdhit.get_sample(8)
         if type(accepted_sample) is str:
             accepted_sample = list(SeqIO.parse(accepted_sample, "fasta"))
         accepted_sample = set([record.id for record in accepted_sample])
-        self.assertEqual(len(accepted_sample), 4)
-        try:
-            accepted_sample.remove("A")
-        except:
-            accepted_sample.remove("B")
-        self.assertEqual(set(accepted_sample), expected_sample)
+        self.assertEqual(len(accepted_sample), 8)
 
     def test_sample_attainable(self):
-        cdhit = CdHit(sequences_path=self.sequences_data_path,
-                      tree=self.tree,
-                      aux_dir=self.aux_dir)
+        cdhit = CdHit(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
         expected_sample = {"A", "C"}
         accepted_sample = cdhit.get_sample(2)
         if type(accepted_sample) is str:
@@ -52,16 +54,16 @@ class TestCDHIT(unittest.TestCase):
         self.assertEqual(accepted_sample, expected_sample)
 
     def test_sample_unattainable(self):
-        cdhit = CdHit(sequences_path=self.sequences_data_path,
-                      tree=self.tree,
-                      aux_dir=self.aux_dir)
-        expected_sample = {"A", "B", "C", "D", "E"}
-        accepted_sample = cdhit.get_sample(5)
+        cdhit = CdHit(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
+        accepted_sample = cdhit.get_sample(6)
         if type(accepted_sample) is str:
             accepted_sample = list(SeqIO.parse(accepted_sample, "fasta"))
         accepted_sample = set([record.id for record in accepted_sample])
-        self.assertEqual(len(accepted_sample), 5)
-        self.assertEqual(accepted_sample, expected_sample)
+        self.assertEqual(len(accepted_sample), 4)
 
 
 if __name__ == "__main__":
--- tests/test_Random.py
+++ tests/test_Random.py
@@ -8,15 +8,18 @@ from samplers import Random
 
 class TestRandom(unittest.TestCase):
 
-    sequences_data_path = f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+    sequences_data_path = (
+        f"{os.path.dirname(os.path.realpath(__file__))}/data/seq_data.fas"
+    )
     aux_dir = f"{os.path.dirname(os.path.realpath(__file__))}/aux/random/"
     tree = Tree("((A:2,B:1):1,(C:1,(D:8,E:2):2):2);")
 
-
     def test_sample_one(self):
-        random_sampler = Random(sequences_path=self.sequences_data_path,
-                                tree=self.tree,
-                                aux_dir=self.aux_dir)
+        random_sampler = Random(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
         accepted_sample = random_sampler.get_sample(1)
         if type(accepted_sample) is str:
             accepted_sample = list(SeqIO.parse(accepted_sample, "fasta"))
@@ -24,9 +27,11 @@ class TestRandom(unittest.TestCase):
         self.assertEqual(len(accepted_sample), 1)
 
     def test_sample_no_repeats(self):
-        random_sampler = Random(sequences_path=self.sequences_data_path,
-                                tree=self.tree,
-                                aux_dir=self.aux_dir)
+        random_sampler = Random(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
         accepted_sample = random_sampler.get_sample(3)
         if type(accepted_sample) is str:
             accepted_sample = list(SeqIO.parse(accepted_sample, "fasta"))
@@ -34,15 +39,16 @@ class TestRandom(unittest.TestCase):
         self.assertEqual(len(accepted_sample), 3)
 
     def test_sample_all(self):
-        random_sampler = Random(sequences_path=self.sequences_data_path,
-                                tree=self.tree,
-                                aux_dir=self.aux_dir)
-        expected_sample = {"A", "B", "C", "D", "E"}
-        accepted_sample = random_sampler.get_sample(5)
+        random_sampler = Random(
+            sequences_path=self.sequences_data_path,
+            tree=self.tree,
+            aux_dir=self.aux_dir,
+        )
+        accepted_sample = random_sampler.get_sample(8)
         if type(accepted_sample) is str:
             accepted_sample = list(SeqIO.parse(accepted_sample, "fasta"))
         accepted_sample = set([record.id for record in accepted_sample])
-        self.assertEqual(accepted_sample, expected_sample)
+        self.assertEqual(len(accepted_sample), 8)
 
 
 if __name__ == "__main__":
