4d4a45c301563ae74542e888ffeb24a73c9daf16
halabikeren <halabikeren@gmail.com>
2021-02-18 10:39:16 +0200

    fixed bug in plotting large scale result

--- src/main_from_simulation.py
+++ src/main_from_simulation.py
@@ -6,9 +6,10 @@ from time import sleep
 import pandas as pd
 import click
 from utils import BaseTools, Job
-from programs import program_to_callable
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import seaborn as sns
+
 load_dotenv()
 
 
@@ -18,11 +19,37 @@ def plot_large_scale_results(df: pd.DataFrame, output_path: str):
     :param output_path: path to plot the data in
     :return: none
     """
+
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    plot = sns.boxplot(y="accuracy", x="sampling_fraction", data=df.groupby(["replicate"]).mean().reset_index(),
-                       palette="colorblind",
-                       hue="sampling_method")
-    plot.savefig(output_path)
+    plt.grid(False)
+    ncols = 2 if not "relative_accuracy_to_ref" in df.columns and "relative_accuracy_to_full" in column else 1
+    fig, axis = plt.subplots(
+        nrows=1,
+        ncols=ncols,
+        sharex="none",
+        sharey="none",
+        figsize=[ncols * 8.5 + 2 + 2, 7.58 + 2],
+        frameon=True,
+    )
+    if "relative_accuracy_to_ref" in df.columns:
+        sns.boxplot(ax=axis[0], y="relative_accuracy_to_ref", x="sampling_fraction", data=df.groupby(["replicate"]).mean().reset_index(),
+                           palette="colorblind",
+                           hue="sampling_method")
+        axis[0].set_ylabel(f"mean relative accuracy ({len(df['replicate'].unique())} replicates)")
+        axis[0].set_xlabel("sampling fraction")
+        axis[0].set_ylim(bottom=0, top=1)
+        axis[0].set_title("reference: simulated rates")
+    if "relative_accuracy_to_full" in df.columns:
+        sns.boxplot(ax=axis[1], y="relative_accuracy_to_full", x="sampling_fraction", data=df.groupby(["replicate"]).mean().reset_index(),
+                           palette="colorblind",
+                           hue="sampling_method")
+        axis[0].set_ylabel(f"mean relative accuracy ({len(df['replicate'].unique())} replicates)")
+        axis[0].set_xlabel("sampling fraction")
+        axis[0].set_title("reference: inferred rates based on full dataset")
+    fig.subplots_adjust()
+    fig.tight_layout()
+    plt.savefig(output_path, bbox_inches="tight", transparent=True)
+    plt.clf()
 
 
 @click.command()
@@ -57,7 +84,8 @@ def exec_pipeline_on_simulations(input_path: click.Path):
     simulations_exist = False
     simulations_exec_complete = False
     repetitions_num = int(simulation_params["nrep"])
-    if os.path.exists(simulation_params['simulations_output_dir']) and os.listdir(simulation_params['simulations_output_dir']) == repetitions_num:
+    if os.path.exists(simulation_params['simulations_output_dir']) and os.listdir(
+            simulation_params['simulations_output_dir']) == repetitions_num:
         simulations_exist = True
         all_exist = True
         for path in os.listdir(simulation_params['simulations_output_dir']):
@@ -70,19 +98,24 @@ def exec_pipeline_on_simulations(input_path: click.Path):
 
     if not simulations_exist:
         pipeline_input_json_paths = BaseTools.simulate(simulation_params=simulation_params)
+        aux_dirs = [f"{os.path.dirname(json_path)}/job_aux/" for json_path in pipeline_input_json_paths]
         logger.info(f"Simulation is complete.")
+
+    else:
+        aux_dirs = [f"{simulation_params['simulations_output_dir']}/{path}/job_aux/" for path in
+                    os.listdir(simulation_params['simulations_output_dir'])]
 
     if not simulations_exec_complete:
         logger.info(f"submitting pipeline jobs for the simulated data")
         completion_validators = []
-        for json_path in pipeline_input_json_paths:
-            aux_dir = f"{os.path.dirname(json_path)}/job_aux/"
+        for aux_dir in aux_dirs:
             if not os.path.exists(f"{aux_dir}/pipeline_on_simulated_data.touch"):
                 job = Job(
                     name="pipeline_on_simulated_data",
                     sh_dir=aux_dir,
                     output_dir=aux_dir,
-                    commands=[f"python /groups/itay_mayrose/halabikeren/down_sampling_analysis/src/main.py --input_path={json_path}"],
+                    commands=[
+                        f"python /groups/itay_mayrose/halabikeren/down_sampling_analysis/src/main.py --input_path={json_path}"],
                     priority=simulation_params["priority"],
                     queue=simulation_params["queue"],
                 )
@@ -111,7 +144,7 @@ def exec_pipeline_on_simulations(input_path: click.Path):
                 logger.error(f"Failed to load dataframe from {df_path} due to error {e}")
         full_df = pd.concat(data)
         # plot large scale data
-        program_to_callable[program].plot_large_scale_results(df=full_df, output_path=output_path)
+        plot_large_scale_results(df=full_df, output_path=output_path)
 
 
 if __name__ == "__main__":
