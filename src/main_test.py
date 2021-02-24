dafe05aa057cbb81828f840c92d21f86fc09a295
halabikeren <halabikeren@gmail.com>
2021-02-24 16:51:00 +0200

    added return exist code

--- src/main_test.py
+++ src/main_test.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 from tests import TestPDA, TestCDHIT, TestRate4ite, TestRandom, TestPAML, TestPipeline, TestSimulationPipeline
 
@@ -9,3 +10,5 @@ for test in TESTS:
     suite.addTests(loader.loadTestsFromTestCase(test))
 runner = unittest.TextTestRunner()
 runner.run(suite)
+ret = not runner.run(suite).wasSuccessful()
+sys.exit(ret)
