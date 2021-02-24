db50e097a9e1a4236b7a9c9ff6bcb6e90aac3d23
halabikeren <halabikeren@gmail.com>
2021-02-24 16:58:28 +0200

    added return code

--- src/main_test.py
+++ src/main_test.py
@@ -1,5 +1,5 @@
-import sys
 import unittest
+import sys
 from tests import TestPDA, TestCDHIT, TestRate4ite, TestRandom, TestPAML, TestPipeline, TestSimulationPipeline
 
 TESTS = [TestCDHIT, TestPDA, TestRandom, TestRate4ite, TestPAML, TestPipeline, TestSimulationPipeline]
@@ -9,6 +9,5 @@ loader = unittest.TestLoader()
 for test in TESTS:
     suite.addTests(loader.loadTestsFromTestCase(test))
 runner = unittest.TextTestRunner()
-runner.run(suite)
 ret = not runner.run(suite).wasSuccessful()
 sys.exit(ret)
