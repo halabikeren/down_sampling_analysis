107c0c45cf794bf91f7e83fd3930d4e9ecc5edce
halabikeren <halabikeren@gmail.com>
2021-03-03 12:51:33 +0200

    added option to run paml from relative path

--- src/programs/paml.py
+++ src/programs/paml.py
@@ -76,10 +76,10 @@ class Paml(Program):
                 self.tree_reconstruction_prams,
             )
         # shorted file paths for paml (gives up upon receiving file paths > 120 chars)
-        shared_dir = f"{os.path.commonprefix([program_input_path, input_tree_path, program_output_path])}/"
-        program_input_path = program_input_path.replace(shared_dir, "/")
-        input_tree_path = input_tree_path.replace(shared_dir, "/")
-        program_output_path = program_output_path.replace(shared_dir, "/")
+        shared_dir = os.path.commonprefix([program_input_path, input_tree_path, program_output_path, control_file_path])
+        program_input_path = program_input_path.replace(shared_dir, "./").replace("//", "/")
+        input_tree_path = input_tree_path.replace(shared_dir, "./").replace("//", "/")
+        program_output_path = program_output_path.replace(shared_dir, "./").replace("//", "/")
 
         self.set_control_file(
             program_input_path,
@@ -89,7 +89,8 @@ class Paml(Program):
             additional_params,
             sequence_data_type=sequence_data_type,
         )
-        return f"cd {shared_dir};{self.program_exe} {control_file_path}"
+        control_file_path = control_file_path.replace(shared_dir, "./").replace("//", "/")
+        return f"cd {shared_dir}\n{self.program_exe} {control_file_path}"
 
     @staticmethod
     def set_control_file(
